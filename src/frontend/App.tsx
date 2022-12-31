05233b8335901be672aebee4fae88a189cead0bb
prizemd2 <daragh.prizeman2@mail.dcu.ie>
2022-12-31 15:11:17 +0000

    Created AWS Amplify project, and User Authentication to app

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {
+  FunctionComponent,
+  useState,
+  useCallback,
+  useEffect,
+} from 'react';
+import {SafeAreaView, Text, Button} from 'react-native';
+import {Amplify, Auth} from 'aws-amplify';
+// @ts-ignore
+import {Authenticator, useAuthenticator} from '@aws-amplify/ui-react-native';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 const App = () => {
+  const [email, setEmail] = useState('');
+
+  const LogOut = () => {
+    const {signOut} = useAuthenticator();
+
+    return (
+      <Button
+        color={'red'}
+        title={'Log out'}
+        onPress={() => {
+          signOut();
+          setEmail('');
+        }}
+      />;
+    );
+  };
+
+  useEffect(() => {
+    console.log('User has changed.');
+  }, [Auth]);
+
+  useEffect(() => {
+    //setUserDetails(Auth.currentUserInfo());
+    // let userDetails = Auth.currentUserInfo();
+    // let authUserDetails = Auth.currentAuthenticatedUser({bypassCache: true})
+    console.log(Auth.user);
+    if (Auth.user != null) {
+      console.log('authUser', Auth.user.attributes.email);
+      setEmail(Auth.user.attributes?.email);
+    } else {
+      setEmail('');
+    }
+  }, [Auth.user]);
+
+  // const login = useCallback(() => {
+  //   Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google});
+  // }, []);
+
   return (
-    <SafeAreaView>
-      <Text>MyAllergyAssistant</Text>
-    </SafeAreaView>
+    <Authenticator.Provider>
+      <Authenticator>
+        <SafeAreaView>
+          <Text>MyAllergyAssistant</Text>
+          <LogOut />
+          <Text>{email}</Text>
+        </SafeAreaView>
+      </Authenticator>
+    </Authenticator.Provider>
   );
 };
 
