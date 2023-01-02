f36a2b7e9601ac865fd679393751e57efeb339f6
George Eskander <george.eskander2@mail.dcu.ie>
2023-01-02 14:23:14 +0000

    Added Jest config file

---
+++ src/frontend/jest.setup.js
@@ -0,0 +1,9 @@
+import mockRNCNetInfo from "@react-native-community/netinfo/jest/netinfo-mock.js";
+
+// NetInfo mock
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
+
+// AsyncStorage mock
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
