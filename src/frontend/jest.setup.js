baf546e752923551a806d1b88b8cda5e50c6b6b9
Daragh Prizeman <daragh.prizeman2@mail.dcu.ie>
2023-01-03 11:48:19 +0000

    Updated jest.setup.js to useFakeTimers()

--- src/frontend/jest.setup.js
+++ src/frontend/jest.setup.js
@@ -1,9 +1,11 @@
-import mockRNCNetInfo from "@react-native-community/netinfo/jest/netinfo-mock.js";
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
 
 // NetInfo mock
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
 
 // AsyncStorage mock
 jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
+
+jest.useFakeTimers();
