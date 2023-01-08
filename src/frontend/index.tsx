5c922684a135126fd60744dfe46970ef78940bed
George Eskander <george.eskander2@mail.dcu.ie>
2023-01-08 15:18:45 +0000

    Added bootsplash, created assets folder, placeholder logo

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,10 +1,17 @@
 import {AppRegistry} from 'react-native';
+import {useEffect} from "react";
 import App from './App';
 import {name as appName} from './app.json';
 import {Authenticator} from '@aws-amplify/ui-react-native';
+import RNBootSplash from "react-native-bootsplash";
 
 
 export default function Index() {
+
+    useEffect(() => {
+        RNBootSplash.hide({fade: true})
+    }, [])
+
     return (
         <Authenticator.Provider>
             <App />
