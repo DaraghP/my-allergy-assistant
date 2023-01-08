1907f44378636514d9f219a73124f6cdee0f1e26
George Eskander <george.eskander2@mail.dcu.ie>
2023-01-08 15:27:50 +0000

    Added mock for testing with bootsplash

--- src/frontend/jest.setup.js
+++ src/frontend/jest.setup.js
@@ -8,4 +8,12 @@ jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
 
+// react-native-bootsplash mock
+jest.mock("react-native-bootsplash", () => {
+  return {
+    hide: jest.fn().mockResolvedValueOnce(),
+    getVisibilityStatus: jest.fn().mockResolvedValue("hidden"),
+  };
+});
+
 jest.useFakeTimers();
