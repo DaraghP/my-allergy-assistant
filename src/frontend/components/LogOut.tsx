209103aa64ec860dc7466f1917ef1bda8a182fde
George Eskander <george.eskander2@mail.dcu.ie>
2023-03-31 18:03:53 +0100

    Added changes to allow creating release APK build for user testing, added name and icon for OS display

--- src/frontend/components/LogOut.tsx
+++ src/frontend/components/LogOut.tsx
@@ -2,7 +2,7 @@ import {Auth} from "aws-amplify";
 import {Button, TouchableOpacity, View} from "react-native";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 
-function LogOut({style = {}, iconColor = "red"}) {
+function LogOut({style = {}, iconColor = "black"}) {
     return (
         <View style={[{padding: 5, borderRadius: 10}, style]}>
             <TouchableOpacity onPress={() => {Auth.signOut()}}>
